a578cd47f67ee46dffd0e1a8ad0863d1302db8ed
sakifabu <sakifabu@live.com>
2016-11-16 23:52:14 -0500

    unit testing

---
+++ README.md
@@ -0,0 +1,2 @@
+"# travis1" 
+"# travis1" 
---
+++ travistest.py
@@ -0,0 +1,33 @@
+import unittest
+
+def function1(var1):
+    return var1 + 1
+
+def function2(var2):
+    return var2 * 2
+
+def functionTrue():
+    return True
+
+def functionFalse():
+    return False
+
+#subclass
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(function1(3), 4)
+
+    def test2(self):
+        self.assertEqual(function2(2), 4)
+
+    def test3(self):
+        self.assertFalse(functionFalse())
+
+    def test4(self):
+        self.assertTrue(functionTrue())
+
+    def test6(self):
+        self.assertRaises(functionTrue())
+
+if __name__ == '__main__':
+    unittest.main()
